603bc863ba9212df533fadab75630c2e73ee0335
labri70 <labri70@naver.com>
2025-03-20 10:04:14 +0900

    파일첨부기능 추가

--- service-worker.js
+++ service-worker.js
@@ -32,7 +32,7 @@ registerRoute(
 
 // API 응답을 stale-while-revalidate 전략으로 캐시합니다.
 registerRoute(
-  ({ url }) => url.origin === 'http://bible-groups.netlify.com',
+  ({ url }) => url.origin === 'https://bible-test.netlify.app',
   new StaleWhileRevalidate({
     cacheName: 'api-responses', // 캐시 이름을 'api-responses'로 설정합니다.
     plugins: [
